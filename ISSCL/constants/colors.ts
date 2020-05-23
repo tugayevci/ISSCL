5a6aab816f348c71d4e4d8cc2272f79be6d38fe3
Tugay Cem Evci <tevci@nuevoyazilim.com>
2020-05-23 18:54:31 +0300

    color pallete added

---
+++ ISSCL/constants/colors.ts
@@ -0,0 +1,15 @@
+const background = "#121212";
+const box = "#2c2c2c";
+const primary = "#BB86FC";
+const primaryVariant = "#3700B3";
+const secondary = "#03DAC6";
+
+const colors = {
+  background,
+  box,
+  primary,
+  primaryVariant,
+  secondary,
+};
+
+export default colors;
